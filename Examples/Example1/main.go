1d02fbd69a2849bc3d9515845b8ca56d4f0039df
Com1 Software, Inc <information@com1software.com>
2023-08-05 16:58:53 -0400

    Create main.go

---
+++ Examples/Example1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"time"
+
+	device "github.com/d2r2/go-hd44780"
+	"github.com/d2r2/go-i2c"
+)
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	i2c, err := i2c.NewI2C(0x27, 1)
+	checkError(err)
+	defer i2c.Close()
+	lcd, err := device.NewLcd(i2c, device.LCD_16x2)
+	checkError(err)
+	err = lcd.BacklightOn()
+	checkError(err)
+	err = lcd.ShowMessage("Test Line 1", device.SHOW_LINE_1)
+	checkError(err)
+	err = lcd.ShowMessage("Test line 2", device.SHOW_LINE_2)
+	checkError(err)
+  err = lcd.ShowMessage("Test line 3", device.SHOW_LINE_2)
+	checkError(err)
+  err = lcd.ShowMessage("Test line 4", device.SHOW_LINE_2)
+	checkError(err)
+
+}
