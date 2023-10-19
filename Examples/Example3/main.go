854496df46c48d779bd00af76d9c4c0c5d0409b7
Dave <information@com1software.com>
2023-10-19 12:20:30 -0400

    update

--- Examples/Example3/main.go
+++ Examples/Example3/main.go
@@ -9,17 +9,27 @@ import (
 
 func main() {
 	l := lcd.New(lcd.LCD{Bus: "/dev/i2c-1", Address: 0x27, Rows: 4, Cols: 20, Backlight: true})
-
+	start := time.Now()
 	if err := l.Init(); err != nil {
 		panic(err)
 	}
 
 	for {
-		if err := l.Print(2, 0, time.Now().Format("02.01.")); err != nil {
+		elapsed := fmt.Sprint(time.Since(start))		
+		if err := l.Print(1, 2, time.Now().Format("Monday Jan 2")); err != nil {
 			fmt.Println(err)
 			return
 		}
-		if err := l.Print(1, 8, time.Now().Format("15:04:05")); err != nil {
+		if err := l.Print(2, 2, time.Now().Format("15:04:05 2006")); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if err := l.Print(3, 2, "Elapsed Time"); err != nil {
+			fmt.Println(err)
+			return
+		}
+		if err := l.Print(4, 2, elapsed); err != nil {
 			fmt.Println(err)
 			return
 		}
