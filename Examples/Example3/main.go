41e0a33ffb07a47fa0ea62894cf0949521b9c8ca
Dave <dave@com1software.com>
2024-02-22 15:37:09 -0500

    update

--- Examples/Example3/main.go
+++ Examples/Example3/main.go
@@ -2,36 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"strings"
+	"time"
 
-	"github.com/mskrha/rpi-lcd"
+	lcd "github.com/mskrha/rpi-lcd"
 )
 
 func main() {
-	l := lcd.New(lcd.LCD{Bus: "/dev/i2c-1", Address: 0x27, Rows: 4, Cols: 20, Backlight: true})
+	l := lcd.New(lcd.LCD{Bus: "/dev/i2c-1", Address: 0x27, Rows: 2, Cols: 16, Backlight: true})
 	start := time.Now()
 	if err := l.Init(); err != nil {
 		panic(err)
 	}
 
 	for {
-		elapsed := fmt.Sprint(time.Since(start))		
-	    e:=strings.Split(elapsed,".")
-		if err := l.Print(1, 2, time.Now().Format("Monday Jan 2")); err != nil {
+		elapsed := fmt.Sprint(time.Since(start))
+		e := strings.Split(elapsed, ".")
+		if err := l.Print(1, 1, time.Now().Format("15:04:05 2006")); err != nil {
 			fmt.Println(err)
 			return
 		}
-		if err := l.Print(2, 2, time.Now().Format("15:04:05 2006")); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if err := l.Print(3, 2, "Elapsed Time"); err != nil {
-			fmt.Println(err)
-			return
-		}
-		if err := l.Print(4, 2, e[0]+"s"); err != nil {
+		if err := l.Print(2, 1, "Elapsed "+e[0]+"s"); err != nil {
 			fmt.Println(err)
 			return
 		}
